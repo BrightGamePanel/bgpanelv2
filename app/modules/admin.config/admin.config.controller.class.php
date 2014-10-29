f5d348d559d8e07455afc81557d2926acfd32c6f
Nikita Rousseau <warhawk3407@gmail.com>
2014-10-29 15:28:42 +0100

    Modules Update

---
+++ app/modules/admin.config/admin.config.controller.class.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * LICENSE:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @categories	Games/Entertainment, Systems Administration
+ * @package		Bright Game Panel V2
+ * @author		warhawk3407 <warhawk3407@gmail.com> @NOSPAM
+ * @copyleft	2014
+ * @license		GNU General Public License version 3.0 (GPLv3)
+ * @version		0.1
+ * @link		http://www.bgpanel.net/
+ */
+
+if ( !class_exists('BGP_Controller')) {
+	trigger_error('Module_Admin_Config -> BGP_Controller is missing !');
+}
+
+/**
+ * Admin Configuration Controller
+ */
+
+class BGP_Controller_Admin_Config extends BGP_Controller {
+
+}
