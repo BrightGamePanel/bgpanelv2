d509d00182262ce2cd7ea0fdf4f20bc7de5d4cad
Darren Rainey <darren@darrenraineys.co.uk>
2015-08-13 19:33:24 +0100

    Added more api functions

---
+++ api/get_server_stats.php
@@ -0,0 +1,10 @@
+CPU Usage <?php include 'get_cpu_load.php';?>
+<br>
+RAM Usage <?php include 'get_ram_usage_precentage.php';?>
+%
+<br>
+<?php include 'get_php_version.php';?>
+<br>
+<?php include 'get_uptime.php';?>
+<br>
+The Panel Version is <?php include 'get_panel_version.php';?>
