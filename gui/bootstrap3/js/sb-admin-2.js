bdfed72cc4e1eb1547f41dbfaf03780f2c644150
Nikita Rousseau <warhawk3407@gmail.com>
2015-08-16 16:44:23 +0200

    GUI: Patch

--- gui/bootstrap3/js/sb-admin-2.js
+++ gui/bootstrap3/js/sb-admin-2.js
@@ -6,7 +6,6 @@ $(function() {
 
 // Loads the correct sidebar on window load,
 // collapses the sidebar on window resize.
-// Sets the min-height of #page-wrapper to window size
 
 $(function() {
     $(window).bind("load resize", function() {
@@ -17,13 +16,6 @@ $(function() {
             topOffset = 100; // 2-row-menu
         } else {
             $('div.navbar-collapse').removeClass('collapse')
-        }
-
-        height = (this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height;
-        height = height - topOffset;
-        if (height < 1) height = 1;
-        if (height > topOffset) {
-            $("#page-wrapper").css("min-height", (height) + "px");
         }
     })
 })
